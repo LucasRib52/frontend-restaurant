eaa4be9e9ce739e66a0c317784008fd3387c6d19
Lucas Cardoso RIbeiro <97406660+LucasRib52@users.noreply.github.com>
2025-05-29 18:39:43 -0300

    adcionando api backend

--- src/services/authApi.js
+++ src/services/authApi.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const authApi = axios.create({
-  baseURL: 'http://192.168.1.65:8000',
+  baseURL: 'https://restaurantesystem.pythonanywhere.com',
   headers: {
     'Content-Type': 'application/json',
   },
